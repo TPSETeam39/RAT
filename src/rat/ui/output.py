c797d22275ada212fc25b3e445805163de3858ee
Yannis <yannis@yannis2707.me>
2026-02-01 19:31:53 +0100

    RAT-37: Use new calculator output format

--- src/rat/ui/output.py
+++ src/rat/ui/output.py
@@ -1,7 +1,7 @@
 import wx
 import wx.dataview
 
-from rat.io import RoleAssignment
+from rat.io import Student, Role
 
 class OutputPanel(wx.Panel):
     COL_ID = 0
@@ -12,35 +12,33 @@ class OutputPanel(wx.Panel):
     def __init__(self, parent: wx.Window):
         super().__init__(parent)
         
-        self.init_layout()
-        self.bind_event_handlers()
-
+        self.list = wx.dataview.TreeListCtrl(self)
         self.list.AppendColumn("ID", flags=wx.dataview.DATAVIEW_COL_SORTABLE | wx.dataview.DATAVIEW_COL_RESIZABLE)
         self.list.AppendColumn("Last Name", flags=wx.dataview.DATAVIEW_COL_SORTABLE | wx.dataview.DATAVIEW_COL_RESIZABLE)
         self.list.AppendColumn("First Name", flags=wx.dataview.DATAVIEW_COL_SORTABLE | wx.dataview.DATAVIEW_COL_RESIZABLE)
         self.list.AppendColumn("Role", flags=wx.dataview.DATAVIEW_COL_SORTABLE | wx.dataview.DATAVIEW_COL_RESIZABLE)
 
-    def init_layout(self):
-        top_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        self._init_layout()
+        self._bind_event_handlers()
 
-        self.list = wx.dataview.TreeListCtrl(self)
-        top_sizer.Add(self.list, 1, wx.EXPAND | wx.ALL, 5)
+    def _init_layout(self):
+        main_sizer = wx.BoxSizer(orient=wx.VERTICAL)
+        main_sizer.Add(self.list, 1, wx.EXPAND | wx.ALL, 5)
+        self.SetSizerAndFit(main_sizer)
 
-        self.SetSizerAndFit(top_sizer)
-
-    def bind_event_handlers(self):
+    def _bind_event_handlers(self):
         pass
 
     def clear(self):
         self.list.DeleteAllItems()
 
-    def load_group(self, group_name: str, assignments: set[RoleAssignment]):
+    def load_group(self, group_name: str, assignments: dict[Student, Role]):
         group_root = self.list.AppendItem(self.list.RootItem, group_name)
 
-        for assignment in assignments:
-            item = self.list.AppendItem(group_root, str(assignment.student.id))
-            self.list.SetItemText(item, self.COL_LAST_NAME, assignment.student.last_name)
-            self.list.SetItemText(item, self.COL_FIRST_NAME, assignment.student.first_name)
-            self.list.SetItemText(item, self.COL_ROLE, assignment.assigned_role.name)
+        for student, role in assignments.items():
+            item = self.list.AppendItem(group_root, str(student.id))
+            self.list.SetItemText(item, self.COL_LAST_NAME, student.last_name)
+            self.list.SetItemText(item, self.COL_FIRST_NAME, student.first_name)
+            self.list.SetItemText(item, self.COL_ROLE, role.name)
         
         self.list.Expand(group_root)
